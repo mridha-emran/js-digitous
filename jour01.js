24236b14778a63d516b68d90b72a0a9e833362f3
mridha-emran <62376427+mridha-emran@users.noreply.github.com>
2021-06-14 15:15:19 +0200

    Add files via upload

---
+++ jour01.js
@@ -0,0 +1,31 @@
+console.log("Hello World !");
+
+var test="My name is emran";
+
+console.log(test); 
+
+var nam ="mridha";
+console.log(test +" "+"Nice to meet you"+" "+nam);
+
+var testLength ="I'm very long !";
+console.log(testLength.length);
+
+var food ="croissant is meh";
+    food=food.replace("meh","so good")
+    console.log(food);
+
+
+    var basic="This is Cool";
+      var basicUp=basic.toUpperCase();
+      var basicDown=basic.toLocaleLowerCase();
+      console.log(basic,basicDown,basicUp);
+
+      var word="banana";
+      var letters=word.split("");
+      console.log(letters);
+
+      var age=30;
+       var template =(`I'm ${age} years old`);
+
+      
+      console.log(template);
