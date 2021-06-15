02840df3f545ed6ec706e00cdcd011a1aa3f43a9
mridha-emran <bari.lala490@gmail.com>
2021-06-15 16:05:35 +0200

    nouvel exercice

---
+++ jour01.js
@@ -0,0 +1,32 @@
+//ex 01
+console.log("Hello World !");
+//EX 02
+var test="My name is emran";
+
+console.log(test); 
+//ex 03
+var nam ="mridha";
+console.log(test +" "+"Nice to meet you"+" "+nam);
+//ex 04
+var testLength ="I'm very long !";
+console.log(testLength.length);
+//EX 05
+var food ="croissant is meh";
+    food=food.replace("meh","so good")
+    console.log(food);
+
+//EX 06
+    var basic="This is Cool";
+      var basicUp=basic.toUpperCase();
+      var basicDown=basic.toLocaleLowerCase();
+      console.log(basic,basicDown,basicUp);
+//EX 07
+      var word="banana";
+      var letters=word.split("");
+      console.log(letters);
+//ex 08
+      var age=30;
+       var template =(`I'm ${age} years old`);
+
+      
+      console.log(template)
---
+++ jour02.js
@@ -0,0 +1,88 @@
+ //ex 1
+ 
+ var integer=102;
+var float=13.9;
+console.log(integer,float);
+
+//ex 2
+var basic=34;
+var stringified=basic.toString();
+console.log(stringified);
+
+//Ex 3
+
+var num = 1.5;
+var rounded=Math.round(num);
+console.log(rounded);
+
+//ex 4
+ var test=12;
+ var bis=5;
+ console(test+bis,test-bis, test%bis);
+// ex 5
+var test = 143;
+var bis =219;
+console.log(test>bis , test<bis,test==bis,test!=bis,test===bis);
+
+//ex 6
+
+
+var limit =50;
+var score=64;
+if(score>=limit){
+    console.log("Ok good !");
+}
+else{
+    console.log("Oh nooo...");
+}
+// ex 8
+var password="azerty";
+if(password.length>5){
+    console.log("The password is secure" );
+}
+//EX 9
+
+if(score>=limit===password.length>5){
+    console.log("Everything is good")
+}
+  else if (score>=limit||password.length>5){
+    console.log("Something is good")
+}
+else if (score>=limit && password.length>5){
+    console.log("nathing is good")
+}
+ var min = 1;
+ var max =6;
+  var random= Math.floor(Math.random() * (max - min + 1) + min); 
+  
+  if(random===6){
+      console.log("yes win");
+  }
+  else{
+      console.log("so close");
+  }
+
+
+  var month="janvier";
+  switch(month){
+      
+    case "janvier":
+          console.log("winter");
+          break;
+          case "mars":
+            console.log("spring");
+            break;
+          case"avril":
+            console.log("summer")  
+            default:
+            console.log("nathing");
+
+  }
+
+  var roundedNumber=3.6;
+  if()
+
+
+
+
+
